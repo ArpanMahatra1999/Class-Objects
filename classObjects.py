c01a71872c3f845a62d6da42a2d48ac377c36a5a
Arpan Mahatra <47106999+ArpanMahatra1999@users.noreply.github.com>
2020-01-18 20:36:02 +0545

    Add files via upload

---
+++ classObjects.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Footballer():
+    pass
+
+Bican = Footballer()
+Pele = Footballer()
+
+Bican.country = "Austria"
+Bican.goals = 800
+
+Pele.country = "Brazil"
+Pele.goals = 700
+
+print(Pele.goals)
+
+
+# In[ ]:
+
+
+
+
